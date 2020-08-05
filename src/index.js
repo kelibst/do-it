2220ebd750b5ceb780161d6092cb899cc3ceb1fb
kelibst <kboster17@gmail.com>
2020-08-05 18:03:55 +0000

    added page click controls

--- src/index.js
+++ src/index.js
@@ -1,36 +1,29 @@
-import spitNum from './card';
-import about from './about';
+const allContainer = document.querySelector('.all-container');
+const addTask = document.querySelector('.addnewTask');
+const taskForm = document.querySelector('.addTask');
+const todoContainer = document.querySelector('.todo-container');
+const addTodobtn = document.querySelector('.addTodobtn'); 
+const taskContainer = document.querySelector('.taskContainer')
+// display addTask form when it is clicked
 
-const moreBtn = document.querySelector('.loader');
-const ul = document.querySelector('.navbar-ul');
-const content = document.getElementById('content');
-const home = document.getElementById('home');
-const abt = document.getElementById('abt');
-
-moreBtn.addEventListener('click', () => {
-  spitNum(5);
+addTask.addEventListener('click', (e) => {
+    allContainer.classList.remove('d-none');
+   todoContainer.classList.remove('d-none');
 });
 
-// loads about 5 cards when the dom loads
-document.addEventListener('DOMContentLoaded', () => {
-  spitNum(5);
-  moreBtn.classList.remove('d-none');
+// hide all forms when the container is clicked
+allContainer.addEventListener('click', e => {
+
+ if(e.target.classList.contains('close-w')){
+   allContainer.classList.add('d-none');
+   todoContainer.classList.add('d-none');
+   taskContainer.classList.add('d-none')
+   
+ }
 });
 
-ul.addEventListener('click', (e) => {
-  if (e.srcElement.id === 'abt') {
-    content.classList.remove('grid-container');
-    e.target.parentElement.classList.add('active');
-    content.innerHTML = '';
-    home.parentElement.classList.remove('active');
-    moreBtn.classList.add('d-none');
-    about(1);
-  } else if (e.srcElement.id === 'home') {
-    content.classList.add('grid-container');
-    content.innerHTML = '';
-    moreBtn.classList.remove('d-none');
-    home.parentElement.classList.add('active');
-    abt.parentElement.classList.remove('active');
-    spitNum(5);
-  }
-});
+addTodobtn.addEventListener('click', () => {
+  allContainer.classList.remove('d-none');
+  taskContainer.classList.remove('d-none')
+})
+
