9c0a7c1ac19e38819f55d367f903a05744ead68c
kelibst <kboster17@gmail.com>
2020-08-06 10:04:17 +0000

    added empty rendering function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import {handleForm, addTask,addTodo, allContainer,todoContainer, taskContainer, taskForm, addTodobtn} from './dom'
-import {Task} from './tasks'
+import {Task, Todo, renderContent, taskTemplate, todoTemplate} from './tasks'
 
 addTodobtn.addEventListener('click', (e) => {
   handleForm().showForm(todoContainer)
@@ -16,16 +16,41 @@ allContainer.addEventListener('click', e => {
 addTask.addEventListener('click', () => {
   handleForm().showForm(taskContainer)
 })
-let alltask =[]
+
+//set default task
+
+let today = Task("Today");
+let alltask =[today];
 taskForm.taskBtn.addEventListener('click', (e) => {
   e.preventDefault();
   let title = taskForm.tasktitle.value;
   let newTask = new Task(title)
   alltask.push(newTask)
+  alltask.forEach(function(index,value){
+  
+    renderContent().render(taskTemplate, renderContent().allTk)
+  })
   console.log(alltask[0])
 })
 
+
+//add initial todos
+let defaultTask = alltask[0];
+defaultTask.todos.push(Todo("Buy milk", 'early in the morning', '11/02/1991','2'))
+
+//add task when the button is clicked
 addTodo.todoBtn.addEventListener('click', (e)=> {
   e.preventDefault();
-  console.log(addTodo.todotitle.value, addTodo.tododesc.value, addTodo.dateofbirth.value, addTodo.priority.value, addTodo.note.value)
+  let name= addTodo.todotitle.value;
+  let description = addTodo.tododesc.value;
+  let dateofbirth = addTodo.dateofbirth.value;
+  let priority =  addTodo.priority.value;
+  let note = addTodo.note.value;
+
+  let setTodo = Todo(name, description, dateofbirth, priority, note);
+  defaultTask.todos.push(setTodo)
+  defaultTask.todos.forEach(function(index,value){
+    renderContent().render(todoTemplate, renderContent().todoTk)
+  })
+  console.log( defaultTask.todos)
 })
--- src/tasks.js
+++ src/tasks.js
@@ -7,6 +7,61 @@ const Task = (name) => ({
     name,
     description,
     duedate,
-    priority
+    priority,
+    note
+    
   });
-export {Task, Todo}
+
+    //set task template
+  let taskTemplate = ` 
+
+                      <div class="card-bins">
+                          <h6 class="card-title py-2 text-uppercase text-ligth">Today</h6>
+                          <i class="fas fa-trash text-danger delete"></i>
+                      </div>
+
+                      <div id="todoTk">
+           
+                      </div>
+
+                      <div class="add-btn text-center">
+                          <a href="#" class="btn addTodobtn btn-outline-primary"><i class="fas fa-plus"></i> Add To do</a>
+                      </div>
+
+                          `
+let todoTemplate = `
+                    <div class="card-bins">
+                    <p class="text-success text-uppercase">Low-Priority</p>
+                    <i class="fas fa-trash ml-2 text-danger delete"></i>
+                    </div>
+
+                    <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quaerat nulla distinctio explicabo voluptate laboriosam pariatur quasi veniam neque eius voluptates.</p>
+                    <a href="#" class="btn btn-outline-success"><i class="fas fa-check" ></i> Done!</a>
+                    <a href="#" class="btn btn-outline-danger"><i class="fas fa-check" ></i> Not Done!</a>
+                  `
+
+//set a function to render all content on the page task or todo                  
+          function renderContent(){
+
+            return {
+             allTk: document.getElementById('allTk'),
+             todoTk: document.getElementById('todoTk'),
+              render(template,  node, container = document.createElement('div')) {
+                if(node === this.allTk){
+                  container.classList = "card shadow col-sm-6 col-lg-4 p-3 round1 border-0 bg-white";
+                }else{
+                  container.classList = "card-body border my-4 round1";
+                }
+                //clear the container before filling it up
+                container.innerHTML = "";
+                container.innerHTML = template;
+                node.appendChild(container);
+              },
+
+
+            }
+          }
+
+      
+
+export {Task, Todo, renderContent, taskTemplate, todoTemplate}
